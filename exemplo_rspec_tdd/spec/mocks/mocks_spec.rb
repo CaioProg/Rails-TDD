b36a88b7c0265f8214bedd5c31b0b5df0a5c74d9
Caio Lucas <caioprog18@gmail.com>
2021-09-13 18:35:47 -0300

    Mocks

---
+++ exemplo_rspec_tdd/spec/mocks/mocks_spec.rb
@@ -0,0 +1,33 @@
+require 'student'
+require 'course'
+
+describe "Mocks" do
+  it '#bar?' do
+    # setup
+    student =  Student.new
+
+    # verify
+    expect(student).to receive(:bar)
+    
+    # exercise
+    student.bar
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
