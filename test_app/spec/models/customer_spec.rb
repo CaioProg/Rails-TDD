67f89b9c818b2b02d1d182fcbbdb0503139689f6
Caio Lucas <caioprog18@gmail.com>
2021-09-14 10:37:20 -0300

    teste

---
+++ test_app/spec/models/customer_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+
+  fixtures :all
+
+  it "Create a Customer" do
+    customer = customers(:jackson)
+
+    expect(customer.full_name).to eq("Sr. Jackson Pires")  
+  end
+end
+  
