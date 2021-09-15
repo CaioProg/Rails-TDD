2fc5759ee3b12fe5bbfd90042462ffa23d8e6ed1
Caio Lucas <caioprog18@gmail.com>
2021-09-15 18:27:27 -0300

    HTTParty

---
+++ test_app/spec/httparty/httparty_spec.rb
@@ -0,0 +1,8 @@
+describe 'HTTparty' do
+    it "HTTparty" do
+        response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+        content_type = response.headers['content-type']
+        expect(content_type).to match(/application\/json/)  
+    end
+    
+end
