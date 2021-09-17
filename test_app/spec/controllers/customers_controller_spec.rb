c39e0f8db8d02110f6027e3a4d84932f7e286f72
Caio Lucas <caioprog18@gmail.com>
2021-09-17 11:03:37 -0300

    Testando Controllers (com autenticação)

--- test_app/spec/controllers/customers_controller_spec.rb
+++ test_app/spec/controllers/customers_controller_spec.rb
@@ -1,13 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe CustomersController, type: :controller do
-    it "responds successfuly" do
-        get :index
-        expect(response).to be_success  
-    end
 
-    it "returns a 200 response" do
-        get :index
-        expect(response).to have_http_status "200"
+  describe "As a Guest" do
+    context "#index" do
+      it "responds successfuly" do
+          get :index
+          expect(response).to be_success  
+      end
+    
+      it "returns a 200 response" do
+          get :index
+          expect(response).to have_http_status "200"
+      end      
     end
+  
+    it "responds a 302 response (not authorized)" do
+      customer = create(:customer)
+      get :show, params: { id: customer.id }
+      expect(response).to have_http_status (302)
+    end
+  end
+  
+describe "As Logged Member" do
+  it "returns a 200 response" do
+    member = create(:member)
+    customer = create(:customer)
+  
+    sign_in member
+  
+    get :show, params: { id: customer.id }
+    expect(response).to have_http_status(200)
+  end
+
+  it "render a :show template" do
+    member = create(:member)
+    customer = create(:customer)
+  
+    sign_in member
+  
+    get :show, params: { id: customer.id }
+    expect(response).to render_template(:show)
+  end
+end
+
 end
