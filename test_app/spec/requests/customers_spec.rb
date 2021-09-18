248263452be672d90eefa9e47e138feb50e9b9f5
Caio Lucas <caioprog18@gmail.com>
2021-09-18 14:16:17 -0300

    RSpec Matchers para include_json

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -11,18 +11,20 @@ RSpec.describe "Customers", type: :request do
         get "/customers.json"
         expect(response).to have_http_status(200)
         expect(response.body).to include_json([
-          id: 1,
-          name: "Joelle VonRueden",
-          email: "Meu_email-1@gmail.com",
-          ])
-    end
+          id: /\d/,
+          name: (be_kind_of String),
+          email: (be_kind_of String),
+        ])
+      end
 
     it "show - JSON 200 OK" do
       get "/customers/1.json"
       expect(response).to have_http_status(200)
       expect(response.body).to include_json(
-        id: 1
-        )
+        id: /\d/,
+        name: (be_kind_of String),
+        email: (be_kind_of String),
+      )
     end
   end
 end
